56dd797979af0b02cd529c3d41f0a56db01585fd
cpooniwala <cpooniwala@gmail.com>
2020-03-10 11:38:51 -0500

    Heroku deployment

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 //Dependencies
 
 const express = require("express");
+const path = require("path");
 
 //Set up Express
 const app = express();
@@ -11,7 +12,9 @@ const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("./public"));
+//app.use(express.static("./public"));
+app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static(path.join(__dirname, "../public")));
 
 //Router
 require("./routes/apiRoutes")(app);
